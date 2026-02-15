bcfc6e97ca8999e8e9fa79885c6a68dfb0954021
Seemahraju9 <145374089+Seemahraju9@users.noreply.github.com>
2026-02-16 00:39:07 +0530

    Update Myfile3.java

--- Myfile3.java
+++ Myfile3.java
@@ -6,6 +6,6 @@ public class Myfile3 {
         // Prints "Hello, World" to the terminal window.
         System.out.println("Hello, World");
         System.out.println("Hello, World");
-System.out.println("Hello, World");
+
     }
 }
